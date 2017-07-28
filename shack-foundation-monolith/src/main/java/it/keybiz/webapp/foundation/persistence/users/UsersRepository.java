e648cd0797da9bc9502ccf527ef786a266c85aba
AndreaLeo <leoa@key-biz.it>
2017-07-28 11:14:01 +0200

    query param added on UsersRepository methods

--- shack-foundation-monolith/src/main/java/it/keybiz/webapp/foundation/persistence/users/UsersRepository.java
+++ shack-foundation-monolith/src/main/java/it/keybiz/webapp/foundation/persistence/users/UsersRepository.java
@@ -1,11 +1,13 @@
 package it.keybiz.webapp.foundation.persistence.users;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 
 import it.keybiz.webapp.foundation.model.User;
 
 public interface UsersRepository extends MongoRepository<User, String>{
 	
-	public User findByUserName(String userName);
-	public User findByEmail(String email);
+	public User findByUserName(@Param(value="userName")String userName);
+	
+	public User findByEmail(@Param(value="email")String email);
 }
